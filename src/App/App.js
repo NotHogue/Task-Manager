5d97c09ba3c16defd4bd397026c86f9286fe279c
Jeremiah Hogue <hogue@Jeremiahs-MBP.attlocal.net>
2023-07-11 12:06:45 -0500

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div>
+      <p>testing123</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+// var webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports = () => {
+    console.log(process.env.NODE_ENV);
+
+    return {
+        entry: [
+            './src/index.js',
+        ],
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            publicPath: '/',
+            filename: 'bundle.js',
+        },
+        mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
+        // devServer: {
+        //     proxy: {
+        //         "/api": "http://localhost:3000",
+        //     }
+        // },
+        module: {
+            rules: [
+                {
+                    test: /\.jsx?/,  // /\.(js|jsx)$/
+                    exclude: /node_modules/,
+                    // use: ['@babel/preset-env', '@babel/preset-react']
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                '@babel/preset-env',
+                                '@babel/preset-react'
+                            ]
+                        }
+                    },
+                },
+                {
+                    test: /.(css|scss)$/,
+                    exclude: /node_modules/,
+                    use: ['style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ],
+                },
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './index.html',
+            }),
+        ]
+    };
+};
